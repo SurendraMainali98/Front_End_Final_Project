571bfdc8a59d28262926a4d5a2fda217c0a24b67
SurendraMainali98 <surendramainali98@gmail.com>
2024-03-28 12:16:42 -0400

    modified and Added different js  function

--- e-com.js
+++ e-com.js
@@ -1,3 +1,4 @@
+
 let icon = document.querySelector(".icon");
 let ul = document.querySelector("ul");
 
@@ -12,61 +13,124 @@ icon.addEventListener("click", ()=>{
     }
 })
 
+
 // navbar
-let shops = doucument.getElementById("shops");
-let reviews = doucument.getElementById("reviews");
-let blogs = doucument.getElementById("blogs");
-let contacts = doucument.getElementById("contacts");
- 
-shops.addEventsListner("click", ()=>{
-    shops.style.color="rgb(4, 219, 219)"
-    reviews.style.color="white"
-    blogs.style.color="white"
-    contacts.style.color="white"
+
+let shops = document.getElementById("shops");
+let reviews = document.getElementById("reviews");
+let blogs = document.getElementById("blogs");
+let contacts = document.getElementById("contacts");
+
+shops.addEventListener("click", ()=>{
+    shops.style.color="rgb(4, 219, 219)";
+    reviews.style.color="white";
+    blogs.style.color="white";
+    contacts.style.color="white";
+
+
 })
 
-reviews.addEventsListner("click", ()=>{
-    reviews.style.color="rgb(4, 219, 219)"
-    shops.style.color="white"
-    blogs.style.color="white"
-    contacts.style.color="white"
+
+reviews.addEventListener("click", ()=>{
+    reviews.style.color="rgb(4, 219, 219)";
+    shops.style.color="white";
+    blogs.style.color="white";
+    contacts.style.color="white";
 })
 
-blogs.addEventsListner("click", ()=>{
+
+blogs.addEventListener("click", ()=>{
     blogs.style.color="rgb(4, 219, 219)"
-    reviews.style.color="white"
-    shops.style.color="white"
-    contacts.style.color="white"
+    reviews.style.color="white";
+    shops.style.color="white";
+    contacts.style.color="white";
 })
 
-contacts.addEventsListner("click", ()=>{
-    contacts.style.color="rgb(4, 219, 219)"
-    reviews.style.color="white"
-    blogs.style.color="white"
-    shops.style.color="white"
+
+contacts.addEventListener("click", ()=>{
+    contacts.style.color="rgb(4, 219, 219)";
+    reviews.style.color="white";
+    blogs.style.color="white";
+    shops.style.color="white";
 })
 
 
 // card js
- 
+
 let crd = document.querySelectorAll(".crd");
-let itemPage = document.querySelector(".itemPage");
+let itemPage = document.querySelector(".itemPage")
 let container = document.querySelector(".container");
 let itemImg = document.getElementById("itemImg");
-let buyBtn = document.getElementById("buyBtn")
+let buyBtn = document.getElementById("buyBtn");
 
 console.log(crd);
 
-crd.forEach(function(curValue) {
-    curValue.addEventListener("click", function() {
-        itemPage.style.display = "flex";
-        container.style.display = "none";
 
-        let imgSrc = curValue.firstElementChild.src;
-        itemImg.src = imgSrc; 
+    crd.forEach(function(curValue){
+         curValue.addEventListener("click", function(){
+            itemPage.style.display="flex";
+            container.style.display="none";
 
-        buyBtn.addEventListener("click",function(){
-            
-        })
+            let imgSrc = curValue.firstElementChild.src ;
+            itemImg.src=imgSrc;
+             
+            let buyText =  document.querySelector(".buyText");
+            buyBtn.addEventListener("click", function(){
+                document.querySelector(".buyPage").style.display="block";
+                buyText.innerHTML=`
+                <h3>Enter Details :</h3>
+                <input type="text" placeholder="Enter Your Name" id="name"> <br>
+                <input type="text" placeholder="Enter Your Address" id="address"> <br>
+                <input type="text" placeholder="Enter Your Mobile Number" id="num"> <br>
+                <h3>Payment Option :</h3>
+                <select>
+                    <option value="Google-Pay">Google-Pay</option>
+                    <option value="Apple-Pay">Apple-Pay</option>
+                    <option value="Bharat-Pay">Credit/Debit</option>
+                    <option value="Cash-on-Delivery">Cash-on-Delivery</option>
+    
+                </select> <br>
+                
+    
+                `
+               let button =  document.createElement("button");
+               button.innerText="Submit";
+               buyText.appendChild(button);
+
+               button.addEventListener("click", function(){
+               let name = document.getElementById("name");
+
+               if(name.value == "" && address.value == "" && num.value == ""){
+                alert("Please Enter Detail")
+               }else{
+                alert("Your Response Recorded");
+                document.querySelector(".buyPage").style.display="none";
+
+               }
+               });
+                 
+                let cross = document.querySelector(".cross");
+                cross.addEventListener("click", function(){
+                document.querySelector(".buyPage").style.display="none";
+
+                });
+            });
+
+             
+         });
+
     });
-});
+ 
+ 
+    // connect
+
+    function connect(){
+        let names = document.getElementById("names");
+        let num = document.getElementById("number");
+        if(name.value == "" && num.value == ""){
+            alert("Fill Details")
+        }else{
+            alert("Thanks For Connecting")
+        }
+
+    }
